294ebf3808e10b2ec66c5ec81469f68cd5a40829
Nikita Panyuhin <79215987151@mail.ru>
2021-02-08 21:15:16 +0300

    Added (and improved existing) scroll arrows

--- marks/marks.js
+++ marks/marks.js
@@ -1,13 +1,21 @@
 var
 	marks = document.getElementsByClassName("marks")[0],
 	table = marks.getElementsByTagName("table")[0],
+
 	spans = table.querySelectorAll("tr:not(:nth-child(1)):not(:nth-child(2)) td span"),
 	table_tr = table.getElementsByTagName("tr"),
-	left_column = document.querySelector(".marks > div > ul:first-child"),
-	left_column_li = left_column.getElementsByTagName("li"),
-	right_column = document.querySelector(".marks > div > ul:last-child"),
-	right_column_li = right_column.getElementsByTagName("li"),
+
+	// left_column = document.querySelector(".marks > div > ul:first-child"),
+	// left_column_li = left_column.getElementsByTagName("li"),
+	
+	// right_column = document.querySelector(".marks > div > ul:last-child"),
+	// right_column_li = right_column.getElementsByTagName("li"),
+
 	details_block = marks.getElementsByClassName("details")[0],
+
+	scroll_left_button = document.getElementById("scroll_left"),
+	scroll_right_button = document.getElementById("scroll_right"),
+
 	table_unlocked = false,
 	details_lock = false;
 
@@ -87,7 +95,7 @@ function onhashchange() {
 		details_lock = false;
 		show_details(task_element);
 
-		(function scroll_table() {
+		(function initial_scroll_table() {
 			if (table_unlocked) {
 				details_lock = true;
 				return;
@@ -99,17 +107,43 @@ function onhashchange() {
 				task_element.getBoundingClientRect().top + html.scrollTop + 10
 			);
 
-			requestAnimationFrame(scroll_table);
+			requestAnimationFrame(initial_scroll_table);
 		})();
 
 	} else {
-		(function scroll_table() {
+		(function initial_scroll_table() {
 			if (table_unlocked) return;
 			table.scrollLeft = table.scrollWidth;
-			requestAnimationFrame(scroll_table);
+			requestAnimationFrame(initial_scroll_table);
 		})();
 	}
 }
+
+
+
+function ScrollParametricBlend(t) {
+    return t * t / (1.85 * t * (t - 1) + 1.0);
+}
+
+function scroll_table_to(target) {
+	let steps_count = 45,
+		step_length = 1 / steps_count,
+        length = target - table.scrollLeft;
+
+    (function scrollStep() {
+        table.scrollLeft = target - ScrollParametricBlend(steps_count * step_length) * length;
+        if (steps_count <= 0) {
+            scroll_animation = false;
+            return;
+        }
+        --steps_count;
+        requestAnimationFrame(scrollStep);
+    })()
+}
+
+function scroll_table_by(distance) {scroll_table_to(table.scrollLeft + distance)}
+
+
 
 Event.add(window, "load", () => {
 	// Event.add(window, "resize", onResize);
@@ -153,6 +187,14 @@ Event.add(window, "load", () => {
 		// Event.add(item, "click", () => {toggle_details_lock(item)});
 	}
 	Event.add(window, "click", toggle_details_lock);
+
+	Event.add(scroll_left_button, "click", () => {
+		scroll_table_by(table.offsetWidth * -0.8);
+	});
+
+	Event.add(scroll_right_button, "click", () => {
+		scroll_table_by(table.offsetWidth * 0.8);
+	});
 
 	setTimeout(() => {
 		table_unlocked = true;
