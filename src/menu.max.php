b74d067fd498c5e586de52bedc823e816f78f722
Nikita Panyuhin <79215987151@mail.ru>
2021-03-03 19:46:09 +0300

    Renamed "Marks" page to "Diary"

--- src/menu.max.php
+++ src/menu.max.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li><a href="/">Главная</a></li>
 		<li><a href="/timetable">Расписание</a></li>
-		<li><a href="/marks">Оценки</a></li>
+		<li><a href="/marks">Дневник</a></li>
 		<li><a href="/announcements">Объявления</a></li>
 		<li><a></a></li>
 		<li><a></a></li>
