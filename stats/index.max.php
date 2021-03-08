790ab38655dd589de0cfa63f96236f86f889d9ec
Nikita Panyuhin <79215987151@mail.ru>
2021-03-08 22:51:10 +0300

    Stats initial commit (part 2)

---
+++ stats/index.max.php
@@ -0,0 +1,68 @@
+<?php
+require_once __DIR__ . '/../src/config.php';
+require_once __DIR__ . '/../src/session.php';
+
+if (!$AUTHORIZED) {
+	logout();
+	redirect('/login/');
+	exit;
+}
+
+$default_mark = 1;
+$default_mark_rate = 10;
+?>
+
+<!DOCTYPE html>
+<html lang="ru"<?php if (isset($_COOKIE['dark']) && $_COOKIE['dark']) echo ' class="dark"'?>>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="build/stats.min.css">
+	<?php include_once __DIR__ . '/../src/favicon.php' ?>
+	<title>NetSchool PTHS | Статистика</title>
+</head>
+<body>
+
+	<?php require_once __DIR__ . '/../src/header.php' ?>
+
+	<main>
+
+		<?php
+		include_once __DIR__ . '/../src/message_alerts.php';
+		require_once __DIR__ . '/../src/menu.html';
+		?>
+
+		<div class="stats">
+			<?php
+
+			$diary = json_decode($person['diary'], true);
+			if (is_null($diary)) {
+				?>
+
+				<p>Оценки не загружены</p>
+
+				<?php
+			} else {
+				?>
+				<div class="tabs">
+					<div class="tab avg_dinamic">
+						<h3>Динамика среднего балла</h3>
+					</div>
+				</div>
+				<?php
+			}
+			?>
+		</div>
+
+	</main>
+
+	<div class="details"></div>
+
+	<script type="text/javascript" src="/src/event.js" defer></script>
+	<script type="text/javascript" src="/src/build/ajax.min.js" defer></script>
+	<script type="text/javascript" src="/src/build/common.min.js" defer></script>
+	<script type="text/javascript" src="build/stats.min.js" defer></script>
+</body>
+
+</html>
