d9acb88f22c5dca1a53250ea2fdfa0dc84cc55e9
Andrew <andr-sitnikov@mail.ru>
2021-03-23 01:34:47 +0300

    Acceptable graphic

--- stats/stats.js
+++ stats/stats.js
@@ -1,28 +1,53 @@
+chart_colors = ["aliceblue", "aqua", "aquamarine", "azure", "beige", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkgrey", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick", "forestgreen", "fuchsia", "gainsboro", "gold", "goldenrod", "gray", "green", "greenyellow", "grey", "hotpink", "indianred", "indigo", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "moccasin", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "yellow", "yellowgreen"]
+
+function random_color(){
+	return chart_colors[Math.floor(Math.random() * chart_colors.length)];
+}
+
+function daysOfYearFromDate(date){
+    return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
+}
+function daysOfYearToDate(day){
+	let date = new Date();
+	date.setMonth(0);
+	date.setDate(day);
+	return date;
+}
+
 window.onload = function() {
+	today = new Date();
+	
 	var drawingCanvas = document.getElementById("dynamics_canvas");
     if(drawingCanvas && drawingCanvas.getContext) {
 		var context = drawingCanvas.getContext('2d');
 		var main_data = JSON.parse(document.getElementById("data").innerText);
 		console.log(main_data);
-		
-		chart_data = {datasets: []};
-		for(lesson in main_data){
-			lesson_data = [];
+		var min_day = 366;
+		var chart_data = {datasets: []};
+		for(let lesson in main_data){
+			var lesson_data = [];
+			let r_color = random_color()
 			//console.log(main_data[lesson]);
-			for(day in main_data[lesson]){
-				lesson_data.push({x: Number(day), y : main_data[lesson][day].toFixed(2)})
-			}
 			
+			for(let day in main_data[lesson]){
+				var last_y = main_data[lesson][day].toFixed(2);
+				let last_x = daysOfYearToDate(Number(day))
+				min_day = Math.min(min_day, day)
+				lesson_data.push({x: last_x, y : last_y})
+			}
+			lesson_data.push({x: today, y : last_y})
 			chart_data["datasets"].push(
-							{label: lesson,
-							borderColor : 'violet',
+							{label: lesson,// + r_color,
+							borderColor : r_color,
 							data: lesson_data,
 							fill : false}
 							)
 			//data[labels].push(lesson);
 		}
 		console.log(chart_data); 
-		
+		let min_date = daysOfYearToDate(min_day);
+		min_date.setDate(0);
+		console.log(min_day, min_date);
 		var myChart = new Chart(context, {
 			type: 'line', 
 			data: chart_data,
@@ -37,9 +62,14 @@ window.onload = function() {
 						}
 					}],
 					xAxes: [{
-						type: 'linear',
+						type: 'time',
+						time: {
+							unit: 'month'
+						},
 						ticks: {
-							//beginAtZero: true
+							max:  today,
+							min: min_date,
+							beginAtZero: true
 						}
 					}]
 				}
