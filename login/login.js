6c9211e0ddd6ac33023a3ee7c5ced4dccd5ff14c
Andrew <andr-sitnikov@mail.ru>
2021-04-13 19:59:23 +0300

    Auto-sign-up (should be tested better, but is quite simple and works with me, so… And I'm too lazy to create a new branch)

--- login/login.js
+++ login/login.js
@@ -6,8 +6,11 @@ var
 	password_input = document.getElementById("password"),
 	message = document.getElementsByClassName("message")[0];
 
-
-Event.add(form, "submit", (e) => {
+var REPR_DELAY = 10000;
+function try_to_sign_in(e, tries_left = 30){ 
+// that means auto-check for 30*10 s = 5 min
+	
+	console.log('registered try', tries_left);
 	e.preventDefault();
 	html.classList.add("wait");
 
@@ -18,7 +21,9 @@ Event.add(form, "submit", (e) => {
 		password = password_input.value.trim();
 
 	message.classList.add("shown");
-
+	
+	
+	
 	ajax(
 		"POST",
 		"/src/login.php", {
@@ -26,12 +31,14 @@ Event.add(form, "submit", (e) => {
 			"password": password
 		},
 		(req) => {
+			console.log('the try itself');
 			// console.log(req.responseText);
 			if (req.responseText == "success") {
 				window.location = "../";
 			
 			} else {
 				r = JSON.parse(req.responseText);
+				console.log(r); 
 				html.classList.remove("wait");
 
 				if (r[0] == "username") {
@@ -39,7 +46,7 @@ Event.add(form, "submit", (e) => {
 					username_input.placeholder = r[1];
 					username_input.value = "";
 					message.classList.remove("shown");
-
+					
 				} else if (r[0] == "password") {
 					password_input.classList.add("invalid");
 					password_input.placeholder = r[1];
@@ -49,8 +56,15 @@ Event.add(form, "submit", (e) => {
 				} else if (r[0] == "message") {
 					// console.log(r);
 					message.classList.add("small");
-					message.getElementsByTagName("p")[0].innerHTML = r[1];
-
+					if (tries_left > 0){
+						message.getElementsByTagName("p")[0].innerHTML = r[1];
+						setTimeout(try_to_sign_in, REPR_DELAY, e, tries_left - 1);
+						
+					}
+					else{
+						message.getElementsByTagName("p")[0].innerHTML = "Аккаунт не найден.<br> Возможно, упал netschool. В противном случае — пароль неверный.";
+					}
+					
 				} else {
 					message.classList.remove("shown");
 					alert("Error");
@@ -65,4 +79,6 @@ Event.add(form, "submit", (e) => {
 			alert(req.responseText);
 		}
 	);
-});
+};
+
+Event.add(form, "submit", try_to_sign_in);
