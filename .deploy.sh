89c264d76d6a41ae7918b8063809e497b771c195
Nikita Panyuhin <79215987151@mail.ru>
2021-04-14 23:35:16 +0300

    Updated deployment

--- .deploy.sh
+++ .deploy.sh
@@ -1,4 +1,5 @@
 git fetch --all
 git reset --hard origin/master
-cp /var/www/netschool/config.json /var/www/netschool/www/src/config/config.json
+chmod -R 755 .
+cp ../config.json ./src/config/config.json
 gulp deploy
